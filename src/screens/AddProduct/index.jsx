2ad9da5c20997738283e25b8e3d0d847383cdaab
beberfabricio <beberfabricio@gmail.com>
2022-12-14 19:02:00 -0300

    se agrega redireccion al login cuando se quiere acceder a una ruta privada

--- src/screens/AddProduct/index.jsx
+++ src/screens/AddProduct/index.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Form from '../../components/Form'
+import { useNavigate } from 'react-router-dom'
 
 const AddProduct = () => {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.data)
+
+  useEffect(() => {
+    if (user.msg !== 'User logged') {
+        navigate('/login');
+    }
+  }, [user, navigate])
+
   const productEmpty = {
     name: '',
     description: '',
--- src/screens/EditProduct/index.jsx
+++ src/screens/EditProduct/index.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Form from '../../components/Form'
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const EditProduct = () => {
+    const navigate = useNavigate();
+    const user = JSON.parse(localStorage.data)
+
+    useEffect(() => {
+        if (user.msg !== 'User logged') {
+            navigate('/login');
+        }
+    }, [user, navigate])
+
     const productsSelector = useSelector((state) => state.products);
+    
+    if (productsSelector.data.length === 0) {
+        return 
+    }
 
     const idToEdit = window.location.pathname.split('/')[3];
 
